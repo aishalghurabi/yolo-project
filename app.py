9702c20dddfc3e7dc4b533a0b79fdeeaceea0fbc
Aisha <alghurabiaisha@gmail.com>
2026-04-02 06:03:16 -0700

    API -> WEB UI

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import os
+import urllib.request
 from model import predict
 
 app = Flask(__name__)
 
+# ---------- HOME PAGE ----------
 @app.route("/")
 def index():
-    return "YOLO API is running"
+    return render_template("index.html")
 
+
+# ---------- PREDICT ----------
 @app.route("/predict", methods=["POST"])
 def run_prediction():
-    if "image" not in request.files:
-        return jsonify({"error": "No image uploaded"})
+    image_url = request.form.get("image_url")
 
-    file = request.files["image"]
+    if not image_url:
+        return "No URL provided"
 
+    # download image
     filepath = "input.jpg"
-    file.save(filepath)
+    urllib.request.urlretrieve(image_url, filepath)
 
+    # run YOLO
     output_path = predict(filepath)
 
-    return jsonify({
-        "result_image": output_path
-    })
-
-
+    # show result in browser
+    return f"""
+    <h2>Detection Result</h2>
+    <img src="/{output_path}" width="500">
+    """
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
